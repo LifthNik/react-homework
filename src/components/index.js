a206af0e1bba7125f579fe81956202a1e2708a0c
LifthNik <hikki20088@gmail.com>
2022-09-25 06:29:21 +0300

    HW4

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export * from './Users/User'
 export * from './Users/userForm'
 export * from './Users/Users'
+
+export * from './Comments/comments'
+export * from './Comments/commentForm'
+export * from './Comments/comment'
---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+export * from './comments/axios.comments'
+export * from './comments/comment.create'
+
+export * from './users/axios.users'
+export * from './users/user.create'
+
