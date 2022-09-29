eef20a6c60a0ece514cf6a544f82cf0ce53889e7
LifthNik <hikki20088@gmail.com>
2022-09-29 06:04:48 +0300

    HW3

--- src/components/index.js
+++ src/components/index.js
@@ -15,6 +15,7 @@ export default function Constructor() {
         <div className="mainWindow">
 
             <Users getUserId={getUserId}/>
+
             <ShowPosts posts={posts}/>
 
         </div>
