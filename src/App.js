b9a67250feba74d38db84550864ada5c6679df56
LifthNik <hikki20088@gmail.com>
2022-09-29 06:00:13 +0300

    HW3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
-import {useState} from "react";
 
-import ShowPosts from "./components/post/Posts";
-import Users from "./components/user/Users";
-import {postService} from "./servises/posts.axios.service";
+import Constructor from "./components";
 
 export default function App() {
-
-    let [posts, setPosts] = useState([])
-
-    let getUserId = (userId) => {
-        postService.getPosts(userId).then(({data}) => setPosts(data))
-    }
 
   return (
     <div className="App">
 
-        <Users getUserId={getUserId}/>
-        <ShowPosts posts={posts}/>
+        <Constructor/>
 
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,22 @@
-export * from 'post/Posts'
-export * from 'post/Post'
+import {useState} from "react";
+import {postService} from "../servises/posts.axios.service";
+import Users from "./user/Users";
+import ShowPosts from "./post/Posts";
 
-export * from 'user/Users'
-export * from 'user/User'
+export default function Constructor() {
+
+    let [posts, setPosts] = useState([])
+
+    let getUserId = (userId) => {
+        postService.getPosts(userId).then(({data}) => setPosts(data))
+    }
+
+    return (
+        <div className="mainWindow">
+
+            <Users getUserId={getUserId}/>
+            <ShowPosts posts={posts}/>
+
+        </div>
+    );
+}
