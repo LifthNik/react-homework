99d97fe9813e7d56715c600334ed80076a43dd22
LifthNik <hikki20088@gmail.com>
2022-09-29 06:12:28 +0300

    HW4

--- src/services/comments/axios.comments.js
+++ src/services/comments/axios.comments.js
@@ -6,8 +6,8 @@ let mainUrl = 'https://jsonplaceholder.typicode.com';
 let axiosComment = axios.create({mainUrl});
 
 let commentsGet = {
-    getComments: () => axiosComment.get(`${mainUrl}/comments?postId=1`),
-    newComment: (comment) => axiosComment.post(`${mainUrl}/comments?postId=1`, comment)
+    getComments: () => axiosComment.get(`${mainUrl}/comments?postId=100`),
+    newComment: (comment) => axiosComment.post(`${mainUrl}/comments?postId=100`, comment)
 }
 
 export {commentsGet};
