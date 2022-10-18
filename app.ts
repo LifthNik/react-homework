c6c3104295ec21a252645b87f65e6e3bce6993f5
LifthNik <hikki20088@gmail.com>
2022-10-18 16:57:03 +0300

    HW_TS

--- app.ts
+++ app.ts
@@ -3,39 +3,38 @@
 // 1) створити інтерфейс на основі цього объекта:
 //     Зверніть увагу там де масиви... в них може бути багато однотипних об'єктів
 
+interface ICores {
+    "flight": number
+    "core": {
+        "reuse_count": number
+        "status": boolean
+    };
+}
+
+interface IPayload {
+    "payload_type": string
+    "payload_mass_kg": number
+    "payload_mass_lbs": number
+}
 
 
 interface Irockets {
-    "mission_name": string,
-    "launch_date_local": string,
+    "mission_name": string
+    "launch_date_local": string
     "launch_site": {
     "site_name_long": string
 },
     "links": {
-    "article_link": null,
+    "article_link": string
         "video_link": string
 },
     "rocket": {
     "rocket_name": string,
         "first_stage": {
-        "cores": [
-            {
-                "flight": number,
-                "core": {
-                    "reuse_count": number,
-                    "status": boolean
-                }
-            }
-        ]
+        "cores": ICores []
     },
     "second_stage": {
-        "payloads": [
-            {
-                "payload_type": string
-                "payload_mass_kg": number
-                "payload_mass_lbs": number
-            }
-        ]
+        "payloads": IPayload []
     }
 }
 }
@@ -73,4 +72,4 @@ function incAge(someUser: iUser, inc: number): iUser {
 
 console.log(sum(1, 2));
 showSum(2,3)
-incAge(user, 2)
+incAge(user, 2);
