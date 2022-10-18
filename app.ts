cc9b07318b0aa2090d2c6ad13b41606565f20574
LifthNik <hikki20088@gmail.com>
2022-10-18 15:10:53 +0300

    HW_TS

--- app.ts
+++ app.ts
@@ -2,41 +2,43 @@
 //
 // 1) створити інтерфейс на основі цього объекта:
 //     Зверніть увагу там де масиви... в них може бути багато однотипних об'єктів
-//
-// {
-//     "mission_name": "Starlink-15 (v1.0)",
-//     "launch_date_local": "2020-10-24T11:31:00-04:00",
-//     "launch_site": {
-//     "site_name_long": "Cape Canaveral Air Force Station Space Launch Complex 40"
-// },
-//     "links": {
-//     "article_link": null,
-//         "video_link": "https://youtu/J442-ti-Dhg"
-// },
-//     "rocket": {
-//     "rocket_name": "Falcon 9",
-//         "first_stage": {
-//         "cores": [
-//             {
-//                 "flight": 7,
-//                 "core": {
-//                     "reuse_count": 6,
-//                     "status": "unknown"
-//                 }
-//             }
-//         ]
-//     },
-//     "second_stage": {
-//         "payloads": [
-//             {
-//                 "payload_type": "Satellite",
-//                 "payload_mass_kg": 15400,
-//                 "payload_mass_lbs": 33951.2
-//             }
-//         ]
-//     }
-// }
-// }
+
+
+
+interface Irockets{
+    "mission_name": string,
+    "launch_date_local": string,
+    "launch_site": {
+    "site_name_long": string
+},
+    "links": {
+    "article_link": null,
+        "video_link": string
+},
+    "rocket": {
+    "rocket_name": string,
+        "first_stage": {
+        "cores": [
+            {
+                "flight": number,
+                "core": {
+                    "reuse_count": number,
+                    "status": boolean
+                }
+            }
+        ]
+    },
+    "second_stage": {
+        "payloads": [
+            {
+                "payload_type": string
+                "payload_mass_kg": number
+                "payload_mass_lbs": number
+            }
+        ]
+    }
+}
+}
 
 // ---------------------------------------------------------------------------—————————
 // 2) протипізувати функції:
@@ -53,15 +55,19 @@
         gender:'male'
     }
 
-function sum(a,b:number):number{
-    return a+b
+function sum(a: number, b: number): number {
+
+    return a + b
 }
-function showSum(a,b: number){
+
+function showSum(a: number, b: number): void {
+
     console.log(a + b);
 }
 
-function incAge(someUser, inc:number):string{
-    someUser.age+=inc
+function incAge(someUser: iUser, inc: number): iUser {
+
+    someUser.age += inc
     return someUser
 }
 
